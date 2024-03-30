f479ce247b7c80d15ed70601b208cb0627995772
Chenjinxuu <160599422+Chenjinxuu@users.noreply.github.com>
2024-03-30 08:02:23 +0000

    final submission
    getting good at this, i hope

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 # Write a function to convert numbers to text numerals
 
 def text_numeral(num):
-    pass
+    """
+    Parameters
+    ---------
+    int
+        an integer number
+    Returns
+    -------
+    str
+        the integer in the word form
+    """
+    count = 0
+    new_num = num
+    NUM_WORD = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninty"]
+    if num <= 20:
+        return NUM_WORD[num-1]
+    elif new_num >= 20:
+        while new_num >= 10:
+            new_num -= 10
+            count += 1
+        if new_num > 0:
+            ones_count = num - count*10
+            return f"{NUM_WORD[17+count]} {NUM_WORD[ones_count-1]}"
+        else:
+            return NUM_WORD[17+count]
